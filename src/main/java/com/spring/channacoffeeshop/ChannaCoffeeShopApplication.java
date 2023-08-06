5d199afe3188aadb9bda917b3b69e51a7bd3af92
Rean Makara <makara.rean@ubill24.com>
2023-08-06 18:19:03 +0700

    first commit

---
+++ src/main/java/com/spring/channacoffeeshop/ChannaCoffeeShopApplication.java
@@ -0,0 +1,13 @@
+package com.spring.channacoffeeshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChannaCoffeeShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChannaCoffeeShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring/channacoffeeshop/ChannaCoffeeShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.channacoffeeshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChannaCoffeeShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
